67e7f999f7e0145e1cbfb80bfafdce758d86e0f8
VidarVigg <vidar.martensson@outlook.com>
2020-05-04 20:17:43 +0200

    Added Timer Functionality

---
+++ Assets/Scripts/Util/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    private bool running;
+    private float eventTime;
+    public delegate void OnTimerDone();
+    public OnTimerDone onTimerDone;
+
+    private void Update()
+    {    
+        if (running)
+        {
+            TimeEvent();
+        }
+    }
+
+    internal void StartTimer(float time)
+    {
+        eventTime = time;
+        running = true;
+    }
+
+    internal void TimeEvent()
+    {
+        eventTime -= Time.deltaTime;
+        Debug.Log(eventTime.ToString("0"));
+        if(eventTime <= 0)
+        {
+            Debug.Log("Timer Done");
+            eventTime = 0;
+            running = false; 
+            onTimerDone?.Invoke();
+            return;
+        }
+    }
+
+}
